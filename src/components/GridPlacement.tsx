a3f21cd2cfc13c721d98113fe6b016f0b19e5d44
Vedeesh Bali <vedeesh18@gmail.com>
2025-06-15 12:52:53 +0100

    functional tabs

---
+++ src/components/GridPlacement.tsx
@@ -0,0 +1,15 @@
+export const gridPlacements = [
+  "1 / 7 / span 2 / span 2",
+  "3 / 5 / span 2 / span 2",
+  "5 / 2 / span 2 / span 3",
+  "5 / 7 / span 2 / span 2",
+  "7 / 1 / span 2 / span 2",
+  "7 / 5 / span 2 / span 4",
+  "9 / 3 / span 2 / span 4",
+  "11 / 1 / span 2 / span 3",
+  "11 / 6 / span 2 / span 3",
+  "13 / 4 / span 2 / span 4",
+  "15 / 1 / span 2 / span 3",
+  "15 / 7 / span 2 / span 2",
+  "17 / 3 / span 2 / span 4",
+];
